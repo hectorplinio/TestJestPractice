82cb7c619c777476be1587a39c11dd4df6e2a710
hectorplinio <hectorplinio@gmail.com>
2022-03-15 16:32:43 +0100

    Create some funtion with axios

--- weather/getWeather.js
+++ weather/getWeather.js
@@ -1,10 +1,12 @@
 const { default: axios } = require("axios");
+require("dotenv").config();
 const url = "https://api.openweathermap.org/data/2.5/weather?q=";
-const ApiKey = "&appid=1fc681a1b30131febd8515b5989e5d95";
+const ApiKey = process.env.API_KEY;
 
 const fetchWeather = async (city) => {
   try {
-    const response = await axios(url + city + ApiKey);
+    debugger;
+    const response = await axios(url + city + "&appid=" + ApiKey);
     return response;
   } catch (error) {
     return error.response;
